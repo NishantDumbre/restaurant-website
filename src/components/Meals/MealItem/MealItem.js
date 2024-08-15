65d69f7e41368c74c55ff8c5354251660562b038
RoyalFlush1010 <nishant.dumbre@gmail.com>
2024-08-15 12:46:23 +0530

    Created a form component and custom Input component

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import classes from "./MealItem.module.css"
+import MealItemForm from './MealItemForm'
 
 const MealItem = (props) => {
 
@@ -13,8 +14,7 @@ const MealItem = (props) => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-                {/* <MealItemForm id={props.id} onAddToCart={addToCartHandler} /> */}
-                <button>asdf</button>
+                <MealItemForm />
             </div>
         </li>
     )
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,13 +1,28 @@
 import React from 'react'
+import Input from '../../UI/Input'
+import classes from "./MealItemForm.module.css"
 
 const MealItemForm = () => {
+
+    const submitHandler = (event) => {
+        event.preventDefault()
+        console.log(event.target)
+    }
+
     return (
         <div>
-            <form>
-                <input type='number'></input>
-                <button type='submit' >Add</button>
+            <form className={classes.form} onSubmit={submitHandler}>
+                <Input label='Amount' input={{
+                    id: 'amount',
+                    type: 'number',
+                    min: 1,
+                    max: 5,
+                    step: 1,
+                    defaultValue: 1
+                }} />
+                <button type='submit'>+ Add</button>
             </form>
         </div>
     )
 }
-    export default MealItemForm
+export default MealItemForm
